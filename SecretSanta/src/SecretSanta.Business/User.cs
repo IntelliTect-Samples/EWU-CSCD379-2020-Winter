35952e8dce867553a1d33c915242afc8eee633ab
Hunter Matthew Rich <xxrichmanxx@gmail.com>
2020-01-08 16:49:07 -0800

    Files and Analyzers Added to Solution

---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    class User
+    {
+        private readonly int ID;
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private ICollection<Gift> Gifts { get; set; }
+    }
+}
