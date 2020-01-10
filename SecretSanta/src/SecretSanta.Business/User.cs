4ecd1881137430949803e0cf5594ea9b51dd96e3
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-10 07:22:49 -0800

    Created User and Gift class along with thier properties and constructors

---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+namespace SecretSanta.Business
+{
+    public class User
+    {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Gift> Gifts { get; set; }
+
+        public User(int id, string firstName,string lastName,List<Gift> gifts)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Gifts = gifts;
+        }
+    }
+}
