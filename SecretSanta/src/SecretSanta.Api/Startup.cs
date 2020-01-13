85993b92da6b8a4b1454ca2173d21fa811e16b61
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-01-12 18:03:47 -0800

    Warnings FIxed and Suppressed

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -5,7 +5,10 @@ using Microsoft.Extensions.Hosting;
 
 namespace SecretSanta.Api
 {
+#pragma warning disable CA1052 // Static holder types should be Static or NotInheritable
+    // referenced elsewhere in a way that depends on it being instantiatable
     public class Startup
+#pragma warning restore CA1052 // Static holder types should be Static or NotInheritable
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -14,7 +17,7 @@ namespace SecretSanta.Api
         //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +30,9 @@ namespace SecretSanta.Api
             {
                 endpoints.MapGet("/", async context =>
                 {
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task, fine to suppress. discussed on gitter
                     await context.Response.WriteAsync("Hello from API!");
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task, fine to suppress. discussed on gitter
                 });
             });
         }
