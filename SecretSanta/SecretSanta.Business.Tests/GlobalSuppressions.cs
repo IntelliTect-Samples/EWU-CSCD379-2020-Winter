861542f13f95d879af9b6ea8956e0a817b47da5e
JerettLatimer <jerettlatimer@yahoo.com>
2020-01-12 23:01:25 -0800

    Fixed Test

--- SecretSanta/SecretSanta.Business.Tests/GlobalSuppressions.cs
+++ SecretSanta/SecretSanta.Business.Tests/GlobalSuppressions.cs
@@ -3,5 +3,5 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test methods can have underscores in their declaration.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test methods can have underscores in their declaration")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Not relevant to this assignment")]
