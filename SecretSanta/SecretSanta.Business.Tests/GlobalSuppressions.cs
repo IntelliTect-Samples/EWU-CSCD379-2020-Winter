6a663f9982e7f28ad0402d86a17c8982d6d7dde1
Caleb <caleb7hawks@gmail.com>
2020-01-14 14:56:02 -0800

    Playing with global suppression

--- SecretSanta/SecretSanta.Business.Tests/GlobalSuppressions.cs
+++ SecretSanta/SecretSanta.Business.Tests/GlobalSuppressions.cs
@@ -3,10 +3,10 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Create_Gift_Success")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Verify_TitlePropertyNotNull_NotNull")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Verify_DescriptionPropertyNotNull_NotNull")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Verify_UrlPropertyNotNull_NotNull")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>")]
+//[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Verify_TitlePropertyNotNull_NotNull")]
+//[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Verify_DescriptionPropertyNotNull_NotNull")]
+//[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.GiftTests.Verify_UrlPropertyNotNull_NotNull")]
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.UserTests.Create_User_Success")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Tests.UserTests.Verify_FirstNamePropertyNotNull_NotNull")]
