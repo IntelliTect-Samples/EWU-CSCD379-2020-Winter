7fc9608cac436d4fe2dce11215470b7ee066a686
Kristofer Veselits <kveselits@eagles.ewu.edu>
2020-01-13 17:33:52 -0800

    Added non-default constructors and supress file for URL warning

--- SecretSanta/src/SecretSanta.Business/Gift.cs
+++ SecretSanta/src/SecretSanta.Business/Gift.cs
@@ -7,5 +7,14 @@
         private string _Description;
         private string _Url;
         private string _User;
+
+        public Gift(int id, string title, string description, string url, string user)
+        {
+            _Id = id;
+            _Title = title;
+            _Description = description;
+            _Url = url;
+            _User = user;
+        }
     }
 }
