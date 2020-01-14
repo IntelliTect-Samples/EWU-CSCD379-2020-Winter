f16db9bb1fbce70affa7958d3cb9a75b762bc1e4
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-01-13 22:57:38 -0800

    Add global warning suppressions

---
+++ SecretSanta/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Assignment specs differ")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Assignment specs differ")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "Not relevant to assignment")]
