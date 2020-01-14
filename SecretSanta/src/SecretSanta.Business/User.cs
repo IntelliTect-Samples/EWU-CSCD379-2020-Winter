9af80b38b7780db579c64b94ad49269d1cd6e5e3
Kristofer Veselits <kveselits@eagles.ewu.edu>
2020-01-13 17:16:59 -0800

    Added Gift and User classes

---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SecretSanta.Business
+{
+    public class User
+    {
+        private readonly int _Id;
+        private string _FirstName;
+        private string _LastName;
+        private Collection<Gift> _Gifts;
+    }
+}
