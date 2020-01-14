ac4581795c58dd33226433bada50466e61eb2d96
Kristofer Veselits <kveselits@eagles.ewu.edu>
2020-01-13 17:16:59 -0800

    Added Gift and User classes

---
+++ SecretSanta/src/SecretSanta.Business/Gift.cs
@@ -0,0 +1,11 @@
+﻿namespace SecretSanta.Business
+{
+    public class Gift
+    {
+        private readonly int _Id;
+        private string _Title;
+        private string _Description;
+        private string _Url;
+        private string _User;
+    }
+}
