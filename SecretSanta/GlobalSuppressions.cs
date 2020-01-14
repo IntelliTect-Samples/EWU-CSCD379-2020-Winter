3988e99c697c632cf30eb4f69bf6947ffda996f5
Kristofer Veselits <kveselits@eagles.ewu.edu>
2020-01-14 14:57:26 -0800

    Correcting global supression justifications

--- SecretSanta/GlobalSuppressions.cs
+++ SecretSanta/GlobalSuppressions.cs
@@ -4,8 +4,8 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 //SecretSanta.Api
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Given code that should not be changed")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "Given code not intended as general purpose")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Given code that should not be changed.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "Given code not intended as general purpose.")]
 //SecretSanta.Business
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Compiler is confusing 'url' with 'uri'")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Compiler is confusing 'url' with 'uri'")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Compiler wants url to not be a string, but supressing this to stay within assignment requirements.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Compiler wants url to not be a string, but supressing this to stay within assignment requirements.")]
