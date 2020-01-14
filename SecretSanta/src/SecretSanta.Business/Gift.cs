32f4aa97b3204f8daea87e477b6bff9093d62da5
HMRich <xxrichmanxx@gmail.com>
2020-01-13 17:50:42 -0800

    Testing Azure Pipelines Testing

--- SecretSanta/src/SecretSanta.Business/Gift.cs
+++ SecretSanta/src/SecretSanta.Business/Gift.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SecretSanta.Business
 {
-    class Gift
+    public class Gift
     {
-        private readonly int ID;
-        private string Title { get; set; }
-        private string Description { get; set; }
-        private string Url { get; set; }
-        private User User { get; set; }
+        private readonly int _Id = 0;
+
+        public int Id
+        {
+            get => _Id;
+        }
+
+        private string _Title = "<Title>";
+
+        public string Title
+        {
+            get => _Title;
+            set => _Title = AssertIsNotNullOrWhitespace(value);
+        }
+
+        private string _Description = "<Descrption>";
+
+        public string Description
+        {
+            get => _Description;
+            set => _Description = AssertIsNotNullOrWhitespace(value);
+        }
+
+        private string _Url = "<Url>";
+
+        public string Url
+        {
+            get => _Url;
+            set => _Url = AssertIsNotNullOrWhitespace(value);
+        }
+
+        private User _User = new User(0, "<fistName>", "<lastName>", new List<Gift>());
+
+        public User User
+        {
+            get => _User;
+            set => _User = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public Gift(int id, string title, string description, string url, User user)
+        {
+            _Id = id;
+            Title = title;
+            Description = description;
+            Url = url;
+            User = user;
+        }
+
+        private string AssertIsNotNullOrWhitespace(string value) =>
+            value switch
+            {
+                null => throw new ArgumentNullException(nameof(value)),
+                "" => throw new ArgumentException($"{nameof(value)} cannot be an empty string.", nameof(value)),
+                string temp when string.IsNullOrWhiteSpace(temp) =>
+                    throw new ArgumentException($"{nameof(value)} cannot be only whitespace.", nameof(value)),
+                _ => value
+            };
+
     }
 }
