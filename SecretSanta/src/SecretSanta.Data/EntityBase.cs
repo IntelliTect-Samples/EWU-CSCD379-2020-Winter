d6ccdae791ad300f9cad9afa2b5baf6dc64d8f35
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-01-16 15:09:43 -0800

    stuff added for part 1

---
+++ SecretSanta/src/SecretSanta.Data/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    class EntityBase
+    {
+        public int Id { get; }
+
+        public EntityBase(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    class FingerPrintEntityBase : EntityBase
+    {
+        public FingerPrintEntityBase(int id) : base(id)
+        {
+            //
+        }
+    }
+}
