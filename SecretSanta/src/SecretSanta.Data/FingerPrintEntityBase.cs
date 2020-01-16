746ec90bc565ff4dff8b1cb731e9f955f106efd4
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-01-16 15:10:28 -0800

    other one stuff

--- SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
+++ SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
@@ -6,9 +6,19 @@ namespace SecretSanta.Data
 {
     class FingerPrintEntityBase : EntityBase
     {
-        public FingerPrintEntityBase(int id) : base(id)
+        public FingerPrintEntityBase(
+            int id, string createdBy, DateTime createdOn, string modifiedBy, DateTime modifiedOn)
+            : base(id)
         {
-            //
+            CreatedBy = createdBy;
+            CreatedOn = createdOn;
+            ModifiedBy = modifiedBy;
+            ModifiedOn = modifiedOn;
         }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
     }
 }
---
+++ SecretSanta/src/SecretSanta.Data/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    class Group : FingerPrintEntityBase
+    {
+        public Group(
+            int id, string createdBy, DateTime createdOn, string modifiedBy, DateTime modifiedOn,
+            string name)
+            : base(id, createdBy, createdOn, modifiedBy, modifiedOn)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
