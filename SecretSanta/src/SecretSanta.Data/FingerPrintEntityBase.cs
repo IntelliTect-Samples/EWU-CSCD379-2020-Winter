9a5e75269874465fe57b58bc666baec27a23f712
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-18 13:41:54 -0800

    Added and completed the EntityBase, FingerPrintEntityBase, and Group classes

---
+++ SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    public class FingerPrintEntityBase : EntityBase
+    {
+        private string _CreatedBy = string.Empty;
+        private string _ModifiedBy = string.Empty;
+
+        public string CreatedBy { get => _CreatedBy; private set => _CreatedBy = value ?? throw new ArgumentNullException(nameof(value)); }
+
+        public DateTime CreatedOn { get; private set; }
+
+        public string ModifiedBy { get => _ModifiedBy; private set => _ModifiedBy = value ?? throw new ArgumentNullException(nameof(value)); }
+
+        public DateTime ModifiedOn { get; private set; }
+    }
+}
