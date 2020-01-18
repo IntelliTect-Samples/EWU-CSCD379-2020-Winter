a5ebb3bf40c2fac32c352ceb608649b04ca74841
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-18 13:48:19 -0800

    Added UserGroup class that holds the many to many relationship between User and Group

---
+++ SecretSanta/src/SecretSanta.Data/UserGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    public class UserGroup
+    {
+#nullable disable
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Group Group { get; set; }
+        public int GroupId { get; set; }
+#nullable enable
+    }
+}
