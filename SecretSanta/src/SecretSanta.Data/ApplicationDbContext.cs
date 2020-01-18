8bb31e05fb8ae70e07595f885029a1150b6ec71e
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-18 15:06:59 -0800

    Added properties to ApplicationDbContext

--- SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
+++ SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
@@ -9,13 +9,32 @@ namespace SecretSanta.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+        public DbSet<Group> Groups { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
 
-        public ApplicationDbContext(DbContextOptions options, IHttpContextAccessor httpContextAccessor)
+        public ApplicationDbContext(DbContextOptions options, IHttpContextAccessor httpContextAccessor) : base(options)
         {
-
+            HttpContextAccessor = httpContextAccessor;
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //implement
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();//implement
+        }
+
+        //public override Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken = default)
+        //{
+
+        //}
     }
 }
