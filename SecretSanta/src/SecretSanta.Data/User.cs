1edc792bf5b993c5b113b1bc5c765aeb563b112a
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-01-21 16:12:33 -0800

    using cleanup

--- SecretSanta/src/SecretSanta.Data/User.cs
+++ SecretSanta/src/SecretSanta.Data/User.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using static SecretSanta.Data.ProjectAsserts;
 
 namespace SecretSanta.Data
