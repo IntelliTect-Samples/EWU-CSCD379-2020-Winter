2b958834e1c324e22a7a424c5407a89cd75b007a
Mark Michaelis <mark@IntelliTect.com>
2020-01-23 01:17:53 -0800

    Refactored out suppression and msbuild files.

--- SecretSanta/test/TestsSuppression.cs
+++ SecretSanta/test/TestsSuppression.cs
@@ -3,6 +3,7 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "Using ConfigureAwait(true) (the default) is acceptable for unit testing.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Allow underscore in test names")]
 
 
