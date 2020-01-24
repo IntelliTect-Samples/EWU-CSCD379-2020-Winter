1c91f8ca30a9e7d929d6e368a56fb278ecad37e2
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-24 10:22:07 -0800

    Changed set privlages for fingerprint data

--- SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
+++ SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
@@ -8,12 +8,12 @@ namespace SecretSanta.Data
     public class FingerPrintEntityBase : EntityBase
     {
         [Required]
-        public string? CreatedBy { get; } 
+        public string? CreatedBy { get; internal set; } 
         [Required]
-        public DateTime CreatedOn { get; }
+        public DateTime? CreatedOn { get; internal set; }
         [Required]
         public string? ModifiedBy { get; internal set; }
         [Required]
-        public DateTime ModifiedOn { get; internal set; }
+        public DateTime? ModifiedOn { get; internal set; }
     }
 }
