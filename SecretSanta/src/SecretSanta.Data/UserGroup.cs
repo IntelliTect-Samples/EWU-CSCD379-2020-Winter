351de4c1718cde6ea1416984d711be3afe26dc53
CDobbins1 <cdobsc01@hotmail.com>
2020-01-23 21:12:06 -0800

    Removed old constructor tests

--- SecretSanta/src/SecretSanta.Data/UserGroup.cs
+++ SecretSanta/src/SecretSanta.Data/UserGroup.cs
@@ -6,10 +6,10 @@ namespace SecretSanta.Data
 {
     public class UserGroup
     {
-#nullable disable
         public int UserId { get; set; }
-        public User User { get; set; }
         public int GroupId { get; set; }
+#nullable disable
+        public User User { get; set; }
         public Group Group { get; set; }
 #nullable enable
     }
--- SecretSanta/test/SecretSanta.Data.Tests/UserTests.cs
+++ SecretSanta/test/SecretSanta.Data.Tests/UserTests.cs
@@ -15,52 +15,6 @@ namespace SecretSanta.Data.Tests
     public class UserTests : TestBase
     {
         [TestMethod]
-        public void User_CanBeCreate_AllPropertiesGetSet()
-        {
-            // Arrange
-            User user = new User
-            {
-                Id = 1,
-                FirstName = "Inigo",
-                LastName = "Montoya",
-                Gifts = new List<Gift>()
-            };
-
-            // Act
-            // Assert
-            Assert.AreEqual(1, user.Id);
-            Assert.AreEqual("Inigo", user.FirstName);
-            Assert.AreEqual("Montoya", user.LastName);
-            Assert.IsNotNull(user.Gifts);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void User_SetFirstNameToNull_ThrowsArgumentNullException()
-        {
-            _ = new User
-            {
-                Id = 1,
-                FirstName = null!,
-                LastName = "Montoya",
-                Gifts = new List<Gift>()
-            };
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void User_SetLastNameToNull_ThrowsArgumentNullException()
-        {
-            _ = new User
-            {
-                Id = 1,
-                FirstName = "Inigo",
-                LastName = null!,
-                Gifts = new List<Gift>()
-            };
-        }
-
-        [TestMethod]
         public async Task CreateUser_ShouldSaveIntoDatabase()
         {
             int userId = -1;
