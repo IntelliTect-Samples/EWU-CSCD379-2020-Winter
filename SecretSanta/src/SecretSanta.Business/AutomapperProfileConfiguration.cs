3d77be5e02e963a6456c15955edb38dbb7c3d038
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-24 16:14:19 -0800

    Created automapper and service interface

---
+++ SecretSanta/src/SecretSanta.Business/AutomapperProfileConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using AutoMapper;
+using SecretSanta.Data;
+
+namespace SecretSanta.Business
+{
+    public class AutomapperProfileConfiguration : Profile
+    {
+        public AutomapperProfileConfiguration()
+        {
+            CreateMap<Gift, Gift>().ForMember(g => g.Id, option => option.Ignore());
+        }
+
+        static public IMapper CreateMapper()
+        {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(Assembly.GetExecutingAssembly());
+            });
+            return mapperConfiguration.CreateMapper();
+        }
+    }
+}
