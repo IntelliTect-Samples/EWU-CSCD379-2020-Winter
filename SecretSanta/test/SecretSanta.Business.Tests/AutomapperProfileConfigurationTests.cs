aac3b28483536d1dabb58c59a38f9e37f6902e1c
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-24 16:25:08 -0800

    Stubbed out SecretSanta.business unit tests

---
+++ SecretSanta/test/SecretSanta.Business.Tests/AutomapperProfileConfigurationTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass]
+    public class AutomapperProfileConfigurationTests
+    {
+    }
+}
