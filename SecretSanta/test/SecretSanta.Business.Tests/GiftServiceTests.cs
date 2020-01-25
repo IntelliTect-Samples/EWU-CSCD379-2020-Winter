acd6c681dcdf94d502055454415e4081ba9c61c6
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-24 16:25:08 -0800

    Stubbed out SecretSanta.business unit tests

---
+++ SecretSanta/test/SecretSanta.Business.Tests/GiftServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass]
+    public class GiftServiceTests
+    {
+    }
+}
