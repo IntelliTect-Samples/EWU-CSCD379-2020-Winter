53ebf363ece4ca2868ea104f1c90a631b05afcca
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-27 13:01:27 -0800

    Fixed service interfaces to match assignment speciifcations

--- SecretSanta/src/SecretSanta.Business/GiftService.cs
+++ SecretSanta/src/SecretSanta.Business/GiftService.cs
@@ -3,39 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace SecretSanta.Business
 {
-    public class GiftService : IEntityService<Gift>
+    public class GiftService : EntityService<Gift>, IGiftService
     {
-        public Task<bool> DeleteAsync(int id)
+        public GiftService(ApplicationDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
         {
-            throw new NotImplementedException();
-        }
 
-        public Task<List<Gift>> FetchAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Gift> FetchByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Gift> InsertAsync(Gift entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Gift[]> InsertAsync(params Gift[] entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Gift> UpdateAsync(int id, Gift entity)
-        {
-            throw new NotImplementedException();
         }
     }
 }
