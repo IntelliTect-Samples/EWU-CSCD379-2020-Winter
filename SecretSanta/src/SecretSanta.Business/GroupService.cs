f8b9f56ad183953957e05733a5d8546f4a3cf888
Caleb <caleb7hawks@gmail.com>
2020-01-28 14:13:16 -0800

    Working on AutoMapper

---
+++ SecretSanta/src/SecretSanta.Business/GroupService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SecretSanta.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Business
+{
+    public class GroupService : EntityService<Group>, IGroupService
+    {
+        public GroupService(ApplicationDbContext context, IMapper mapper) :
+            base(context, mapper)
+        { }
+    }
+}
