27fdeb689c840a7e69695a1ed9a331040bc25f6a
Cooperwutzke <36475473+Cooperwutzke@users.noreply.github.com>
2020-01-28 15:32:59 -0800

    Test Run
    With suppression of warnings.

--- SecretSanta/SolutionSuppression.cs
+++ SecretSanta/SolutionSuppression.cs
@@ -1,4 +1,5 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "<Pending>")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Error caused by System.URI looking for characters matching URL, url, etc. If variable does not represent a URI warning is safe to supress according to Microsoft Documentation")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1812:AutomapperProfileConfiguration is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic)", Justification = "Can I make it static? I simply don't know. Until then this doesn't present any major issues")]
 
