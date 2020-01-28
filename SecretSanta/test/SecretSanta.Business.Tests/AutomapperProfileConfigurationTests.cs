78257c4c925607de8f617dca5346e926076f3bb4
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-01-28 13:36:42 -0800

    Added unit test for AutoMapper

--- SecretSanta/test/SecretSanta.Business.Tests/AutomapperProfileConfigurationTests.cs
+++ SecretSanta/test/SecretSanta.Business.Tests/AutomapperProfileConfigurationTests.cs
@@ -2,11 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using AutoMapper;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
 
 namespace SecretSanta.Business.Tests
 {
     [TestClass]
     public class AutomapperProfileConfigurationTests
     {
+        [TestMethod]
+        public void CreateMapper_AllMaps_ValidConfiguration()
+        {
+            //Arrange
+            //Act
+            IMapper mapper = AutomapperProfileConfiguration.CreateMapper();
+            var config = mapper.ConfigurationProvider;
+            //Assert
+            config.AssertConfigurationIsValid();
+        }
     }
 }
