4eae474f9f1a10ddbc74c204729d07c9e0a3f001
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-01-29 01:12:17 -0800

    Update Gift tests

--- SecretSanta/test/SecretSanta.Data.Tests/GiftTests.cs
+++ SecretSanta/test/SecretSanta.Data.Tests/GiftTests.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SecretSanta.Data.Tests
@@ -11,29 +14,105 @@ namespace SecretSanta.Data.Tests
         [TestMethod]
         public async Task Gift_CanBeSavedToDatabase()
         {
-            // Arrange
+            Gift gift = SampleData.Gift1;
+            string title = gift.Title;
+            string url = gift.Url;
+            string desc = gift.Description;
+
             using (var dbContext = new ApplicationDbContext(Options))
             {
-                dbContext.Gifts.Add(
-                    new Gift(
-                        "Ring Doorbell", "The doorbell that saw too much",
-                        "www.ring.com", new User("Inigo", "Montoya")
-                    )
-                );
+                dbContext.Gifts.Add(gift);
                 await dbContext.SaveChangesAsync().ConfigureAwait(false);
             }
-            // Act
-            // Assert
+
             using (var dbContext = new ApplicationDbContext(Options))
             {
                 var gifts = await dbContext.Gifts.ToListAsync();
 
                 Assert.AreEqual(1, gifts.Count);
-                Assert.AreEqual("Ring Doorbell", gifts[0].Title);
-                Assert.AreEqual("www.ring.com", gifts[0].Url);
-                Assert.AreEqual("The doorbell that saw too much", gifts[0].Description);
+                Assert.AreEqual(title, gifts[0].Title);
+                Assert.AreEqual(url, gifts[0].Url);
+                Assert.AreEqual(desc, gifts[0].Description);
             }
         }
+
+        [TestMethod]
+        public async Task Gift_HasFingerPrintDataAddedOnInitialSave()
+        {
+            User user = SampleData.User1;
+            string identifier = $"{user.LastName}, {user.FirstName}";
+
+            Gift gift = SampleData.Gift1;
+
+            IHttpContextAccessor httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta =>
+                hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, identifier));
+
+            using (var dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                dbContext.Gifts.Add(gift);
+                await dbContext.SaveChangesAsync().ConfigureAwait(false);
+            }
+
+            using (var dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var gifts = await dbContext.Gifts.ToListAsync();
+
+                Assert.AreEqual(1, gifts.Count);
+                Assert.AreEqual(identifier, gifts[0].CreatedBy);
+                Assert.AreEqual(identifier, gifts[0].ModifiedBy);
+            }
+        }
+
+        [TestMethod]
+        public async Task Gift_HasFingerPrintDataUpdateOnUpdate()
+        {
+            User user1 = SampleData.User1;
+            string identifier1 = $"{user1.LastName}, {user1.FirstName}";
+
+            User user2 = SampleData.User3;
+            string identifier2 = $"{user2.LastName}, {user2.FirstName}";
+
+            Gift gift1 = SampleData.Gift1;
+
+            Gift gift2 = SampleData.Gift2;
+            string title = gift2.Title;
+            string desc = gift2.Description;
+            string url = gift2.Url;
+
+            IHttpContextAccessor httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta =>
+                hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, identifier1));
+
+            using (var dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                dbContext.Gifts.Add(gift1);
+                await dbContext.SaveChangesAsync().ConfigureAwait(false);
+            }
+
+            httpContextAccessor = Mock.Of<IHttpContextAccessor>(hta =>
+                    hta.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier) == new Claim(ClaimTypes.NameIdentifier, identifier2));
+
+            using (var dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var gifts = await dbContext.Gifts.ToListAsync();
+
+                Assert.AreEqual(1, gifts.Count);
+                gifts[0].Title = title;
+                gifts[0].Description = desc;
+                gifts[0].Url = url;
+
+                await dbContext.SaveChangesAsync().ConfigureAwait(false);
+            }
+
+            using (var dbContext = new ApplicationDbContext(Options, httpContextAccessor))
+            {
+                var gifts = await dbContext.Gifts.ToListAsync();
+
+                Assert.AreEqual(1, gifts.Count);
+                Assert.AreEqual(identifier1, gifts[0].CreatedBy);
+                Assert.AreEqual(identifier2, gifts[0].ModifiedBy);
+            }
+        }
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void Gift_SetTitleToNull_ThrowsArgumentNullException()
