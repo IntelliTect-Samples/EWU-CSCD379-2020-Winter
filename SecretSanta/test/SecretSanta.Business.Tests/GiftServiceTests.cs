cda8e1a4f544f85033923591144037d48952e6e4
Caleb <caleb7hawks@gmail.com>
2020-01-28 22:57:35 -0800

    Finished some more tests

--- SecretSanta/test/SecretSanta.Business.Tests/GiftServiceTests.cs
+++ SecretSanta/test/SecretSanta.Business.Tests/GiftServiceTests.cs
@@ -35,5 +35,64 @@ namespace SecretSanta.Business.Tests
             Assert.AreSame(gift.User, user);
             Assert.AreEqual(user.Id, gift.User.Id);
         }
+
+        [TestMethod]
+        public async Task FetchByIdGift_ShouldIncludeUser()
+        {
+            // Arrange
+            using var dbContext = new ApplicationDbContext(Options);
+
+            IGiftService service = new GiftService(dbContext, Mapper);
+
+            var user = new User("Billy", "Bob");
+
+            var gift = new Gift("Title", "Description", "www.url.com", user);
+
+            await service.InsertAsync(gift);
+
+            // Act
+
+            // Assert
+            using var dbContext2 = new ApplicationDbContext(Options);
+            service = new GiftService(dbContext, Mapper);
+            gift = await service.FetchByIdAsync(gift.Id!.Value);
+
+            Assert.IsNotNull(gift.User);
+        }
+
+        [TestMethod]
+        public async Task UpdateUser_ShouldSaveIntoDatabase()
+        {
+            // Arrange
+            using var dbContext = new ApplicationDbContext(Options);
+
+            IUserService service = new UserService(dbContext, Mapper);
+
+            var user = new User("Billy", "Bob");
+
+            var user2 = new User("Jimmy", "John");
+
+            await service.InsertAsync(user);
+            await service.InsertAsync(user2);
+
+            // Act
+            using var dbContext2 = new ApplicationDbContext(Options);
+            User fetchUser = await dbContext2.Users.SingleAsync(item => item.Id == user.Id);
+
+            fetchUser.FirstName = "Billy";
+            fetchUser.LastName = "Bob";
+
+            using var dbContext3 = new ApplicationDbContext(Options);
+            var service2 = new UserService(dbContext3, Mapper);
+            await service2.UpdateAsync(2, fetchUser);
+
+            // Assert
+            using var dbContext4 = new ApplicationDbContext(Options);
+            User savedUser = await dbContext4.Users.SingleAsync(item => item.Id == user.Id);
+            var otherAuthor = await dbContext4.Users.SingleAsync(item => item.Id == 2);
+            Assert.AreEqual(("Billy", "Bob"), (savedUser.FirstName, savedUser.LastName));
+            Assert.AreNotEqual((savedUser.FirstName, savedUser.LastName), (otherAuthor.FirstName, otherAuthor.LastName));
+
+        }
     }
 }
---
+++ SecretSanta/test/SecretSanta.Business.Tests/UserServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass]
+    public class UserServiceTests : TestBase
+    {
+        [TestMethod]
+        public async Task InsertAsync_BillyAndFred_Success()
+        {
+            // Arrange
+            using var dbContextInsert = new ApplicationDbContext(Options);
+            IUserService service = new UserService(dbContextInsert, Mapper);
+
+            User billy = SampleData.CreateBillyBob();
+            User fred = SampleData.CreateFredFlintstone();
+
+            // Act
+            await service.InsertAsync(billy);
+            await service.InsertAsync(fred);
+
+            // Assert
+            Assert.IsNotNull(billy.Id);
+            Assert.IsNotNull(fred.Id);
+        }
+
+        [TestMethod]
+        public async Task UpdateUser_ShouldSaveIntoDatabase()
+        {
+            // Arrange
+            using var dbContextInsert = new ApplicationDbContext(Options);
+            IUserService service = new UserService(dbContextInsert, Mapper);
+
+            User billy = SampleData.CreateBillyBob();
+            User fred = SampleData.CreateFredFlintstone();
+
+            await service.InsertAsync(billy);
+            await service.InsertAsync(fred);
+
+            // Act
+            using var dbContextFetch = new ApplicationDbContext(Options);
+            User billyFromDb = await dbContextFetch.Users.SingleAsync(item => item.Id == billy.Id);
+
+            const string updatedLastName = "Not Billy Bob at all";
+            billyFromDb.LastName = updatedLastName;
+
+            // Update Billy Bob using Fred Id.
+            await service.UpdateAsync(fred.Id!.Value, billyFromDb);
+
+            // Assert
+            using var dbContextAssert = new ApplicationDbContext(Options);
+            billyFromDb = await dbContextAssert.Users.SingleAsync(item => item.Id == billy.Id);
+            var fredFromDb = await dbContextAssert.Users.SingleAsync(item => item.Id == 2);
+
+
+            // I'm confused about how this test is supposed to work based off the lecture notes
+            //Assert.AreEqual(
+            //    (SampleData.Billy, updatedLastName), (fredFromDb.FirstName, fredFromDb.LastName));
+
+            //Assert.AreEqual(
+            //    (SampleData.Billy, SampleData.Bob), (billyFromDb.FirstName, billyFromDb.LastName));
+        }
+    }
+}
