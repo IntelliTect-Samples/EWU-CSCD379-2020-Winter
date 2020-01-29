d69e1ba54a944cb5219e148a0f6d939d672dcee9
Brett <gamer.bah@live.com>
2020-01-28 18:32:15 -0800

    Finished Encapsulation

--- SecretSanta/src/SecretSanta.Data/Gift.cs
+++ SecretSanta/src/SecretSanta.Data/Gift.cs
@@ -4,17 +4,57 @@ using System.Text;
 
 namespace SecretSanta.Data
 {
+
     public class Gift : FingerPrintEntityBase
     {
-        public string Title { get => _Title; set => _Title = value ?? throw new ArgumentNullException(nameof(Title)); }
+
+        public string Title
+        {
+            get => _Title;
+            set => _Title = value ?? throw new ArgumentNullException(nameof(Title));
+        }
+
         private string _Title = string.Empty;
-        public string Description { get => _Description; set => _Description = value ?? throw new ArgumentNullException(nameof(Description)); }
+
+        public string Description
+        {
+            get => _Description;
+            set => _Description = value ?? throw new ArgumentNullException(nameof(Description));
+        }
+
         private string _Description = string.Empty;
-        public string Url { get => _Url; set => _Url = value ?? throw new ArgumentNullException(nameof(Url)); }
+
+        public string Url
+        {
+            get => _Url;
+            set => _Url = value ?? throw new ArgumentNullException(nameof(Url));
+        }
+
         private string _Url = string.Empty;
 #nullable disable
         public User User { get; set; }
 #nullable enable
         public int UserId { get; set; }
+
+        public Gift(string title, string description, string url, User user) : this(title,
+                                                                                    description,
+                                                                                    url,
+// Unable to check for nullability in constructor chain
+#pragma warning disable CA1062 // Validate arguments of public methods
+                                                                                    user.Id)
+#pragma warning restore CA1062 // Validate arguments of public methods
+        {
+            User = user;
+        }
+
+        private Gift(string title, string description, string url, int userId)
+        {
+            Title       = title;
+            Description = description;
+            Url         = url;
+            UserId      = userId;
+        }
+
     }
+
 }
