ee10d3a5b4d2685c30f5ee7c4c8298977834bca1
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-01-29 02:55:30 -0800

    Add test base

---
+++ SecretSanta/test/SecretSanta.Business.Tests/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.Business.Tests
+{
+    public class TestBase : SecretSanta.Data.Tests.TestBase
+    {
+        // Justification: Set by InitializeTests
+#nullable disable // CS8618: Non-nullable field is uninitialized. Consider declaring as nullable.
+        protected IMapper Mapper { get; private set; }
+#nullable enable
+
+        [TestInitialize]
+        public override void InitializeTests()
+        {
+            base.InitializeTests();
+            Mapper = AutomapperProfileConfiguration.CreateMapper();
+        }
+    }
+}
