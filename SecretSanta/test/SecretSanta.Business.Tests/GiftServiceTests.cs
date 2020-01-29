f20e28b6bd031982f12e0601bef602cb55e4df74
Caleb <caleb7hawks@gmail.com>
2020-01-28 22:18:26 -0800

    Writing tests for services

---
+++ SecretSanta/test/SecretSanta.Business.Tests/GiftServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass]
+    public class GiftServiceTests : TestBase
+    {
+        [TestMethod]
+        public async Task CreateGift_ShouldSaveIntoDatabase()
+        {
+            // Arrange
+            using var dbContext = new ApplicationDbContext(Options);
+
+            IGiftService service = new GiftService(dbContext, Mapper);
+
+            var user = new User("Billy", "Bob");
+
+            var gift = new Gift("Title", "Description", "www.url.com", user);
+
+            await service.InsertAsync(gift);
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(gift.Id);
+            Assert.IsNotNull(user.Id);
+            Assert.AreSame(gift.User, user);
+            Assert.AreEqual(user.Id, gift.User.Id);
+        }
+    }
+}
--- SecretSanta/test/SecretSanta.Business.Tests/GroupServiceTests.cs
+++ SecretSanta/test/SecretSanta.Business.Tests/GroupServiceTests.cs
@@ -1,13 +1,67 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SecretSanta.Business.Tests
 {
     [TestClass]
     public class GroupServiceTests : TestBase
     {
+        [TestMethod]
+        public async Task InsertAsync_CoolAndCrazy_Success()
+        {
+            // Arrange
+            using var dbContextInsert = new ApplicationDbContext(Options);
+            IGroupService service = new GroupService(dbContextInsert, Mapper);
 
+            var crazyGroup = SampleData.CreateCrazyGroup();
+            var coolGroup = SampleData.CreateCoolGroup();
+
+            // Act
+            await service.InsertAsync(crazyGroup);
+            await service.InsertAsync(coolGroup);
+
+            // Assert
+            Assert.IsNotNull(crazyGroup.Id);
+            Assert.IsNotNull(coolGroup.Id);
+        }
+
+        [TestMethod]
+        public async Task UpdateGroup_ShouldSaveIntoDatabase()
+        {
+            // Arrange
+            using var dbContextInsert = new ApplicationDbContext(Options);
+            IGroupService service = new GroupService(dbContextInsert, Mapper);
+
+            var crazyGroup = SampleData.CreateCrazyGroup();
+            var coolGroup = SampleData.CreateCoolGroup();
+
+            await service.InsertAsync(crazyGroup);
+            await service.InsertAsync(coolGroup);
+
+            // Act
+            using var dbContextFetch = new ApplicationDbContext(Options);
+            Group crazyGroupFromDb = await dbContextFetch.Groups.SingleAsync(item => item.Id == crazyGroup.Id);
+
+            const string extremeTitle = "Extreme group";
+            crazyGroupFromDb.Title = extremeTitle;
+
+            // Update Crazy Group using the Cool Groups Id.
+            await service.UpdateAsync(coolGroup.Id!.Value, crazyGroupFromDb);
+
+            // Assert
+            using var dbContextAssert = new ApplicationDbContext(Options);
+            crazyGroupFromDb = await dbContextAssert.Groups.SingleAsync(item => item.Id == crazyGroup.Id);
+            Group coolGroupFromDb = await dbContextAssert.Groups.SingleAsync(item => item.Id == 2);
+
+            Assert.AreEqual(SampleData.CrazyGroupName, crazyGroupFromDb.Title);
+            //Assert.AreEqual(extremeTitle, coolGroupFromDb.Title);
+
+        }
     }
 }
