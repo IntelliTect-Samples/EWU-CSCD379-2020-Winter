a161b4c15e1461a025f2d4e3ec7c07abeead54c0
Kris Veselits <kveselits@eagles.ewu.edu>
2020-01-28 23:36:31 -0800

    Fixed namespace typo

--- SecretSanta/src/SecretSanta.Api/Program.cs
+++ SecretSanta/src/SecretSanta.Api/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BlogEngine.Api
+namespace SecretSanta.Api
 {
     static public class Program
     {
--- SecretSanta/src/SecretSanta.Web/Program.cs
+++ SecretSanta/src/SecretSanta.Web/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BlogEngine.Web
+namespace SecretSanta.Web
 {
     static public class Program
     {
