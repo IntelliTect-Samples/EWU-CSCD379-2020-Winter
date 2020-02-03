5aa9c27329a47dbd1f732b51248f5df1fba7d221
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-02-03 15:53:17 -0800

    Addressed some DI warnings

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -18,7 +18,7 @@ namespace SecretSanta.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             var sqliteConnection = new SqliteConnection("DataSource=:memory:");
             sqliteConnection.Open();
