a49c87c421cb5f81c035f2d1d2d2fb062e515b30
JerettLatimer <jerettlatimer@yahoo.com>
2020-02-03 13:20:49 -0800

    Created Unit Tests

---
+++ SecretSanta/test/SecretSanta.Api.Tests/Controllers/GiftControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests.Controllers
+{
+    [TestClass]
+    public class GiftControllerTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Api.Tests/UserControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
