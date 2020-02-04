51560b91d360356a5ab678356bc955f154ba123c
JerettLatimer <jerettlatimer@yahoo.com>
2020-02-03 18:58:31 -0800

    Finished UserControllerTests and GroupControllerTests. Assignment should be complete.

--- SecretSanta/test/SecretSanta.Api.Tests/Controllers/GroupControllerTests.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/Controllers/GroupControllerTests.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Business;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SecretSanta.Api.Tests.Controllers
 {
@@ -9,9 +16,109 @@ namespace SecretSanta.Api.Tests.Controllers
     public class GroupControllerTests
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void Create_GroupController_Success()
         {
+            // Arrange
+            Mock<IGroupService> service = new Mock<IGroupService>();
 
+            // Act
+            _ = new GroupController(service.Object);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Create_WithoutService_Fails()
+        {
+            _ = new GroupController(null!);
+        }
+
+        [TestMethod]
+        public async Task Post_Group_Success()
+        {
+            // Arrange
+            Mock<IGroupService> service = new Mock<IGroupService>();
+            Group redTeam = SampleData.CreateRedTeam;
+            service.Setup(g => g.InsertAsync(redTeam)).ReturnsAsync(redTeam);
+            GroupController controller = new GroupController(service.Object);
+
+            // Act
+            redTeam = await controller.Post(redTeam);
+
+            // Assert
+            Assert.IsNotNull(redTeam.Id);
+        }
+
+        [TestMethod]
+        public async Task Put_Group_Success()
+        {
+            // Arrange
+            var service = new Mock<IGroupService>();
+            Group group = SampleData.CreateRedTeam;
+            TestGroup testGroup = new TestGroup(group, group.Id);
+            service.Setup(g => g.UpdateAsync(group.Id, group)).ReturnsAsync(testGroup);
+            service.Setup(g => g.FetchByIdAsync(group.Id)).ReturnsAsync(testGroup);
+            var controller = new GroupController(service.Object);
+
+            // Act
+            ActionResult<Group> result = await controller.Put(group.Id, group);
+            OkObjectResult okResult = (OkObjectResult)result.Result;
+            Group gResult = (Group)okResult.Value;
+
+            // Assert
+            Assert.IsTrue(result.Result is OkObjectResult);
+            Assert.AreEqual(gResult.Id, group.Id);
+        }
+
+        [TestMethod]
+        public async Task Fetch_Group_Success()
+        {
+            // Arrange
+            Mock<IGroupService> service = new Mock<IGroupService>();
+            Group redTeam = SampleData.CreateRedTeam;
+            Group blueTeam = SampleData.CreateBlueTeam;
+            List<Group> groups = new List<Group>();
+            groups.Add(redTeam);
+            groups.Add(blueTeam);
+            service.Setup(g => g.FetchAllAsync()).ReturnsAsync(groups);
+            GroupController controller = new GroupController(service.Object);
+
+            // Act
+            List<Group> returnGroups = (List<Group>)await controller.Get();
+
+            // Assert
+            Assert.AreEqual(2, returnGroups.Count);
+        }
+
+        [TestMethod]
+        public async Task Delete_Group_Success()
+        {
+            // Arrange
+            Mock<IGroupService> service = new Mock<IGroupService>();
+            Group redTeam = SampleData.CreateRedTeam;
+            TestGroup testGroup = new TestGroup(redTeam, redTeam.Id);
+            service.Setup(g => g.DeleteAsync(redTeam.Id)).ReturnsAsync(true);
+            service.Setup(g => g.FetchByIdAsync(redTeam.Id)).ReturnsAsync(testGroup);
+            GroupController controller = new GroupController(service.Object);
+
+            // Act
+            ActionResult<bool> result = await controller.Delete(redTeam.Id);
+            OkObjectResult okResult = (OkObjectResult)result.Result;
+            bool gResult = (bool)okResult.Value;
+
+            // Assert
+            Assert.IsTrue(result.Result is OkObjectResult);
+            Assert.AreEqual(true, gResult);
+        }
+
+        private class TestGroup : Group
+        {
+            public TestGroup(Group group, int id)
+                : base(group.Title)
+            {
+                Id = id;
+            }
         }
     }
 }
--- SecretSanta/test/SecretSanta.Api.Tests/Controllers/UserControllerTests.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/Controllers/UserControllerTests.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Business;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SecretSanta.Api.Tests
 {
@@ -9,9 +16,109 @@ namespace SecretSanta.Api.Tests
     public class UserControllerTests
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void Create_UserController_Success()
         {
+            // Arrange
+            Mock<IUserService> service = new Mock<IUserService>();
 
+            // Act
+            _ = new UserController(service.Object);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Create_WithoutService_Fails()
+        {
+            _ = new UserController(null!);
+        }
+
+        [TestMethod]
+        public async Task Post_User_Success()
+        {
+            // Arrange
+            Mock<IUserService> service = new Mock<IUserService>();
+            User user = SampleData.CreateInigoMontoya();
+            service.Setup(g => g.InsertAsync(user)).ReturnsAsync(user);
+            UserController controller = new UserController(service.Object);
+
+            // Act
+            user = await controller.Post(user);
+
+            // Assert
+            Assert.IsNotNull(user.Id);
+        }
+
+        [TestMethod]
+        public async Task Put_User_Success()
+        {
+            // Arrange
+            var service = new Mock<IUserService>();
+            User user = SampleData.CreateJerettLatimer();
+            TestUser testUser = new TestUser(user, user.Id);
+            service.Setup(g => g.UpdateAsync(user.Id, user)).ReturnsAsync(testUser);
+            service.Setup(g => g.FetchByIdAsync(user.Id)).ReturnsAsync(testUser);
+            var controller = new UserController(service.Object);
+
+            // Act
+            ActionResult<User> result = await controller.Put(user.Id, user);
+            OkObjectResult okResult = (OkObjectResult)result.Result;
+            User gResult = (User)okResult.Value;
+
+            // Assert
+            Assert.IsTrue(result.Result is OkObjectResult);
+            Assert.AreEqual(gResult.Id, user.Id);
+        }
+
+        [TestMethod]
+        public async Task Fetch_User_Success()
+        {
+            // Arrange
+            Mock<IUserService> service = new Mock<IUserService>();
+            User jerett = SampleData.CreateJerettLatimer();
+            User inigo = SampleData.CreateInigoMontoya();
+            List<User> users = new List<User>();
+            users.Add(jerett);
+            users.Add(inigo);
+            service.Setup(g => g.FetchAllAsync()).ReturnsAsync(users);
+            UserController controller = new UserController(service.Object);
+
+            // Act
+            List<User> returnUsers = (List<User>)await controller.Get();
+
+            // Assert
+            Assert.AreEqual(2, returnUsers.Count);
+        }
+
+        [TestMethod]
+        public async Task Delete_User_Success()
+        {
+            // Arrange
+            Mock<IUserService> service = new Mock<IUserService>();
+            User user = SampleData.CreateInigoMontoya();
+            TestUser testUser = new TestUser(user, user.Id);
+            service.Setup(g => g.DeleteAsync(user.Id)).ReturnsAsync(true);
+            service.Setup(g => g.FetchByIdAsync(user.Id)).ReturnsAsync(testUser);
+            UserController controller = new UserController(service.Object);
+
+            // Act
+            ActionResult<bool> result = await controller.Delete(user.Id);
+            OkObjectResult okResult = (OkObjectResult)result.Result;
+            bool gResult = (bool)okResult.Value;
+
+            // Assert
+            Assert.IsTrue(result.Result is OkObjectResult);
+            Assert.AreEqual(true, gResult);
+        }
+
+        private class TestUser : User
+        {
+            public TestUser(User user, int id)
+                : base(user.FirstName, user.LastName)
+            {
+                Id = id;
+            }
         }
     }
 }
