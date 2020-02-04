b1626549b5301d72a7e44db1d49e76f5357cd7f5
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-02-03 20:15:15 -0800

    Implement base controller

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/EntityController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Business;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class EntityController<T> : ControllerBase where T : class
+    {
+        private IEntityService<T> EntityService { get; }
+
+        protected EntityController(IEntityService<T> entityService)
+        {
+            EntityService = entityService ?? throw new System.ArgumentNullException(nameof(entityService));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<T>> Get()
+        {
+            List<T> entities = await EntityService.FetchAllAsync();
+            return entities;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<T>> Get(int id)
+        {
+            if (await EntityService.FetchByIdAsync(id) is T entity)
+            {
+                return Ok(entity);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<T>> Post([FromBody] T value)
+        {
+            T? entity = await EntityService.InsertAsync(value);
+
+            if (entity is T e) return Ok(e);
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<T>> PutAsync(int id, [FromBody] T value)
+        {
+            T? entity = await EntityService.UpdateAsync(id, value);
+
+            if (entity is T e) return Ok(e);
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            bool success = await EntityService.DeleteAsync(id);
+
+            if (success) return Ok();
+
+            return NotFound();
+        }
+    }
+}
+
