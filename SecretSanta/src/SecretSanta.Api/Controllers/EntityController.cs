a888599453ab1868c25978a5afd16a3d4b46eed7
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-02-03 17:13:13 -0800

    Reworked controllers to abuse polymorphism and then implemented all controllers

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/EntityController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Business;
+using SecretSanta.Business.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecretSanta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class EntityController<TEntity> : Controller where TEntity : class
+    {
+        private IEntityService<TEntity> EntityService { get; }
+
+        public EntityController(IEntityService<TEntity> entityService)
+        {
+            EntityService = entityService;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IEnumerable<TEntity>> Get()
+        {
+            List<TEntity> entities = await EntityService.FetchAllAsync();
+            return entities;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TEntity>> Get(int id)
+        {
+            if(await EntityService.FetchByIdAsync(id) is TEntity entity)
+            {
+                return Ok(entity);
+            }
+            return NotFound();
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<TEntity> Post(TEntity value)
+        {
+            return await EntityService.InsertAsync(value);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TEntity>> Put(int id, TEntity value)
+        {
+            if(await EntityService.UpdateAsync(id,value) is TEntity entity)
+            {
+                return Ok(entity);
+            }
+            return NotFound();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if(await EntityService.DeleteAsync(id) is true)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
--- SecretSanta/src/SecretSanta.Api/Controllers/GroupController.cs
+++ SecretSanta/src/SecretSanta.Api/Controllers/GroupController.cs
@@ -12,64 +12,11 @@ using SecretSanta.Data;
 namespace SecretSanta.Api.Controllers
 {
     [Route("api/[controller]")]
-    public class GroupController : Controller
+    public class GroupController : EntityController<Group>
     {
-        private IGroupService GroupService { get; }
+        public GroupController(IGroupService groupService) : base(groupService)
+        {
 
-        public GroupController(IGroupService groupService)
-        {
-            GroupService = groupService;
-        }
-        // GET: api/<controller>
-        [HttpGet]
-        public async Task<IEnumerable<Group>> Get()
-        {
-            List<Group> Groups = await GroupService.FetchAllAsync();
-            return Groups;
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<Group>> Get(int id)
-        {
-            if(await GroupService.FetchByIdAsync(id) is Group group)
-            {
-                return Ok(group);
-            }
-            return NotFound();
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public async Task<Group> Post(Group value)
-        {
-            return await GroupService.InsertAsync(value);
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<Group>> Put(int id, Group value)
-        {
-            if(await GroupService.UpdateAsync(id, value) is Group group)
-            {
-                return Ok(group);
-            }
-            return NotFound();
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
-        {
-            if(await GroupService.DeleteAsync(id) == true)
-            {
-                return Ok();
-            }
-            return NotFound();
         }
     }
 }
