48ecbf98dddcc0855ce8fe1f540418499c3a1842
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-02-04 02:44:32 -0800

    Add group controller tests

---
+++ SecretSanta/test/SecretSanta.Api.Tests/Controllers/GroupControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Business;
+using SecretSanta.Data;
+using System;
+
+namespace SecretSanta.Api.Tests.Controllers
+{
+    [TestClass]
+    public class GroupControllerTests : EntityControllerTests<Group>
+    {
+        protected override EntityController<Group> CreateController =>
+            new GroupController(new MockGroupService());
+
+        protected override Group CreateEntity =>
+            new Group(Guid.NewGuid().ToString());
+    }
+
+    internal class IdGroup : Group
+    {
+        public IdGroup(string title, int id) : base(title) =>
+            Id = id;
+    }
+
+    internal class MockGroupService : MockEntityService<Group>, IGroupService
+    {
+        protected override Group AddId(Group group, int id) =>
+            new IdGroup(group.Title, id);
+    }
+}
