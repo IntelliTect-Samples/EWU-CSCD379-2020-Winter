8eedb97c4cdd5fa7caf21422a7a576ee08ce4fe1
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-02-04 02:37:18 -0800

    Add service mock

---
+++ SecretSanta/test/SecretSanta.Api.Tests/MockService.cs
@@ -0,0 +1,37 @@
+﻿using SecretSanta.Business;
+using SecretSanta.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.Tests
+{
+    internal abstract class MockEntityService<TEntity> : IEntityService<TEntity>
+        where TEntity : EntityBase
+    {
+        private IDictionary<int, TEntity> Data { get; } = new Dictionary<int, TEntity>();
+
+        protected abstract TEntity AddId(TEntity entity, int id);
+
+        public Task<bool> DeleteAsync(int id) =>
+            Task.FromResult(Data.Remove(id));
+
+        public Task<List<TEntity>> FetchAllAsync() =>
+            Task.FromResult(Data.Values.ToList());
+
+        public Task<TEntity> FetchByIdAsync(int id) =>
+            Task.FromResult(Data.TryGetValue(id, out var entity) ? entity : null!);
+
+        public Task<TEntity> InsertAsync(TEntity entity)
+        {
+            TEntity add = AddId(entity, Data.Count);
+            Data[add.Id] = add;
+            return Task.FromResult(add);
+        }
+
+        public Task<TEntity?> UpdateAsync(int id, TEntity entity) =>
+            Task.FromResult(Data.ContainsKey(id) ? Data[id] = entity : null);
+    }
+}
+
