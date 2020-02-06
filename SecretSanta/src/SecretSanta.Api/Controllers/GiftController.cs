5c937a61826ff0336ca823c4ab24f646f556bc7c
Caleb <caleb7hawks@gmail.com>
2020-02-06 10:49:02 -0800

    Added Dto's for Entities

--- SecretSanta/src/SecretSanta.Api/Controllers/GiftController.cs
+++ SecretSanta/src/SecretSanta.Api/Controllers/GiftController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SecretSanta.Business;
+using SecretSanta.Business.Services;
+using SecretSanta.Business.Dto;
 using SecretSanta.Data;
 
 namespace SecretSanta.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GiftController : BaseApiController<Gift>
+    public class GiftController : BaseApiController<Business.Dto.Gift, Business.Dto.GiftInput>
     {
         public GiftController(IGiftService giftService)
             : base (giftService)
---
+++ SecretSanta/src/SecretSanta.Business/Dto/GiftInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business.Dto
+{
+    public class GiftInput
+    {
+        public string? Title { get; set; }
+        public string? Url { get; set; }
+        public string? Description { get; set; }
+    }
+}
