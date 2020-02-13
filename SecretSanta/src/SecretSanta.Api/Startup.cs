e6b3d2d31d846d5b65c25e0b308767b9694cbf89
Caleb <caleb7hawks@gmail.com>
2020-02-13 12:18:07 -0800

    Set up migrations and routing

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SecretSanta.Business;
@@ -15,21 +16,24 @@ namespace SecretSanta.Api
     public class Startup
 #pragma warning restore CA1052 // Static holder types should be Static or NotInheritable
     {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public static void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.EnableSensitiveDataLogging()
+                    .UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddSwaggerDocument();
-
             services.AddScoped<IGiftService, GiftService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IGroupService, GroupService>();
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.EnableSensitiveDataLogging()
-                       .UseSqlite("Data Source=SecretSanta.db"));
-
             services.AddAutoMapper(new[] { typeof(AutomapperConfigurationProfile).Assembly });
         }
 
@@ -43,8 +47,8 @@ namespace SecretSanta.Api
 
             app.UseOpenApi();
             app.UseSwaggerUi3();
-
             app.UseMvc();
+            app.UseEndpoints(endpoint => endpoint.MapDefaultControllerRoute());
         }
     }
 }
