de92fc00f24c11555f309ccc1f999f2563979d56
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-02-13 12:07:37 -0800

    Remove the hardcoded Data Source value for the database and use the value stored in the ConnectionStrings.DefaultConnection within the appropriate App Settings file

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SecretSanta.Business;
@@ -15,11 +16,16 @@ namespace SecretSanta.Api
     public class Startup
 #pragma warning restore CA1052 // Static holder types should be Static or NotInheritable
     {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public static void ConfigureServices(IServiceCollection services)
+        public  void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddControllers();
             services.AddSwaggerDocument();
 
             services.AddScoped<IGiftService, GiftService>();
@@ -28,7 +34,7 @@ namespace SecretSanta.Api
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.EnableSensitiveDataLogging()
-                       .UseSqlite("Data Source=SecretSanta.db"));
+                       .UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddAutoMapper(new[] { typeof(AutomapperConfigurationProfile).Assembly });
         }
@@ -44,7 +50,10 @@ namespace SecretSanta.Api
             app.UseOpenApi();
             app.UseSwaggerUi3();
 
-            app.UseMvc();
+            app.UseEndpoints(enpoint =>
+            {
+                enpoint.MapDefaultControllerRoute(); 
+            });
         }
     }
 }
--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -13,8 +13,7 @@ namespace SecretSanta.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public static void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
-
+            services.AddControllersWithViews();  
             services.AddHttpClient("SecretSantaApi");
         }
 
@@ -28,9 +27,15 @@ namespace SecretSanta.Web
 
             app.UseHttpsRedirection();
 
+            app.UseRouting();
+
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapDefaultControllerRoute();
+            });
+
         }
     }
 }
