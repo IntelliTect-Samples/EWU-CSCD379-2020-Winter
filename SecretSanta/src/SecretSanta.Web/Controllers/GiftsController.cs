ffaf6c9649a5e54319a48dc4872e026f2c646cf9
Kenny <jessetuan@comcast.net>
2020-02-17 16:59:27 -0800

    Working on Assignment 6

--- SecretSanta/src/SecretSanta.Web/Controllers/GiftsController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/GiftsController.cs
@@ -9,18 +9,85 @@ namespace SecretSanta.Web.Controllers
 {
     public class GiftsController : Controller
     {
+        public IHttpClientFactory ClientFactory { get; }
+
         public GiftsController(IHttpClientFactory clientFactory)
         {
             HttpClient httpClient = clientFactory?.CreateClient("SecretSantaApi") ?? throw new ArgumentNullException(nameof(clientFactory));
             Client = new GiftClient(httpClient);
+            ClientFactory = clientFactory;
         }
 
         private GiftClient Client { get; }
 
-        public async Task<IActionResult> Index()
+        public async Task<ActionResult> Index()
         {
             ICollection<Gift> gifts = await Client.GetAllAsync();
             return View(gifts);
+
+            //HttpClient httpClient = ClientFactory.CreateClient("BlogApi");
+
+            //var client = new GiftClient(httpClient);
+            //ICollection<Gift> authors = await client.GetAllAsync();
+            //return View(authors);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(GiftInput giftInput)
+        {
+            ActionResult result = View(giftInput);
+
+            if(ModelState.IsValid)
+            {
+                await Client.PostAsync(giftInput);
+                result = RedirectToAction(nameof(Index));
+            }
+
+            return result;
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            ActionResult result = View(id);
+
+            if (ModelState.IsValid)
+            {
+                Gift retrievedGift = await Client.GetAsync(id);
+                result = View(retrievedGift);
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, GiftInput giftInput)
+        {
+            ActionResult result = View();
+
+            if (ModelState.IsValid)
+            {
+                await Client.PutAsync(id, giftInput);
+                result = RedirectToAction(nameof(Index));
+            }
+
+            return result;
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Client.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            Gift retrievedGift = await Client.GetAsync(id);
+            return View(retrievedGift);
         }
     }
 }
