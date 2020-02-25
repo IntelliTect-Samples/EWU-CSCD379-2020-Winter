31cc99415acd97c5c76f5927f2ef6093a0463972
Caleb <caleb7hawks@gmail.com>
2020-02-25 14:46:12 -0800

    Working on fixing webpack

---
+++ SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
@@ -0,0 +1,59 @@
+﻿
+
+
+export class App {
+
+    async renderGifts() {
+      //  document.write("Hello World");
+        console.log("hello world")
+        //var gifts = await this.retrieveGifts();
+        //const giftList = document.getElementById("giftList");
+        //gifts.forEach(gift => {
+        //    const listItem = document.createElement("li");
+        //    listItem.textContent = `${gift.id}:${gift.title}:${gift.description}:${gift.url}`
+        //    giftList.append(listItem);
+        //})
+    }
+
+    //giftClient: IGiftClient;
+    //constructor(giftClient: IGiftClient = new GiftClient()) {
+    //    this.giftClient = giftClient;
+    //}
+
+    async createGiftList() {
+        //await this.deleteGifts();
+
+        //var user = new User({
+        //    firstName: "Billy",
+        //    lastName: "Bob",
+        //    santaId: null,
+        //    gifts: null,
+        //    groups: null,
+        //    id: 1
+        //});
+
+        //let gifts: Gift[];
+        //for (var i = 0; i < 5; i++) {
+        //    var gift = new Gift({
+        //        title: "Cool Gift",
+        //        description: "Description",
+        //        url: "http://www.cool.com",
+        //        userId: 1,
+        //        id: i
+        //    })
+        //    this.giftClient.post(gift);
+        //}
+    }
+
+    async deleteGifts() {
+        //var gifts = await this.retrieveGifts();
+        //for (var i = 0; i < gifts.length; i++) {
+        //    await this.giftClient.delete(gifts[i].id);
+        //}
+    }
+
+    async retrieveGifts() {
+        //var gifts = await this.giftClient.getAll();
+        //return gifts;
+    }
+}
