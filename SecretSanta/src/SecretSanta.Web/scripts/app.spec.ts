015a1b315d97fc581fdddd5ec8ed400046706b9a
Aaron Sauther <aaron.sauther@eagles.ewu.edu>
2020-02-25 00:36:44 -0800

    Implemented getAll in the MockGiftClient

--- SecretSanta/src/SecretSanta.Web/scripts/app.spec.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/app.spec.ts
@@ -1,4 +1,41 @@
 ﻿import { App } from './list-Gifts';
 import { expect } from 'chai';
 import 'mocha';
-import { IGiftClient, GiftClient, IGroupClient, GroupClient, IUserClient, UserClient } from "./secret-santa-api.client"
+import { IGiftClient, GiftClient, User, Gift} from "./secret-santa-api.client"
+
+class MockGiftClient implements IGiftClient {
+    async getAll(): Promise<Gift[]> {
+        var user = new User({
+            firstName: "Spongebob",
+            lastName: "Squarepants",
+            id: 42,
+            santaId: null,
+            gifts: null,
+            groups: null
+        });
+        let gifts: Gift[];
+        for (var i = 0; i < 5; i++) {
+            Gift[i] = new Gift({
+                title: "LeSpatula",
+                description: "Le Spatula 3000",
+                id: i + 1,
+                url: "https://spongebob.fandom.com/wiki/Le_Spatula",
+                userId: user.id
+            });    
+        }
+        return gifts;
+    }
+    post(entity: import("./secret-santa-api.client").GiftInput): Promise<import("./secret-santa-api.client").Gift> {
+        throw new Error("Method not implemented.");
+    }
+    get(id: number): Promise<import("./secret-santa-api.client").Gift> {
+        throw new Error("Method not implemented.");
+    }
+    put(id: number, value: import("./secret-santa-api.client").GiftInput): Promise<import("./secret-santa-api.client").Gift> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: number): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+}
--- SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
@@ -1,20 +1,25 @@
-﻿import { IGiftClient, GiftClient, IGroupClient, GroupClient, IUserClient, UserClient } from "./secret-santa-api.client"
+﻿import { IGiftClient, GiftClient } from "./secret-santa-api.client"
 
 export class App
 {
     async renderGifts() {
-        var gifts = await this.giftClient.getAll();
+        var gifts = await this.getAllGifts();
         const giftsPage = document.getElementById("giftsPage");
         for (let index = 0; index < gifts.length; index++) {
             const gift = gifts[index];
             const listItem = document.createElement("li");
             listItem.textContent = '${gift.title}: ${gift.description}: ${gift.url}';
             giftsPage.append(listItem);
-        }
+        } 
     }
 
     giftClient: IGiftClient;
     constructor(giftClient: IGiftClient = new GiftClient()) {
         this.giftClient = giftClient; 
+    }
+
+    async getAllGifts() {
+        var gifts = await this.giftClient.getAll();
+        return gifts;
     }
 }
