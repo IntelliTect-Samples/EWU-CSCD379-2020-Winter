f96fd164e7adffe4fbc7aaae4b2693445399d948
Caleb <caleb7hawks@gmail.com>
2020-02-27 15:34:29 -0800

    Fixed csproj file, got Home to redirect to ListGifts and got Gifts to display

--- SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/list-Gifts.ts
@@ -1,59 +1,67 @@
-﻿
+﻿import {
+    IGiftClient,
+    GiftClient,
+    User,
+    Gift
+} from "./secretsanta-engine.client";
 
 
 export class App {
 
     async renderGifts() {
       //  document.write("Hello World");
-        console.log("hello world")
-        //var gifts = await this.retrieveGifts();
-        //const giftList = document.getElementById("giftList");
-        //gifts.forEach(gift => {
-        //    const listItem = document.createElement("li");
-        //    listItem.textContent = `${gift.id}:${gift.title}:${gift.description}:${gift.url}`
-        //    giftList.append(listItem);
-        //})
+     //   console.log("hello world")
+        var gifts = await this.retrieveGifts();
+        const giftList = document.getElementById("giftList");
+        gifts.forEach(gift => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${gift.id}:${gift.title}:${gift.description}:${gift.url}`
+            giftList.append(listItem);
+        })
     }
 
-    //giftClient: IGiftClient;
-    //constructor(giftClient: IGiftClient = new GiftClient()) {
-    //    this.giftClient = giftClient;
-    //}
+    giftClient: IGiftClient;
+    constructor(giftClient: IGiftClient = new GiftClient()) {
+        this.giftClient = giftClient;
+    }
 
     async createGiftList() {
-        //await this.deleteGifts();
+        await this.deleteGifts();
+        await this.createUser();
 
-        //var user = new User({
-        //    firstName: "Billy",
-        //    lastName: "Bob",
-        //    santaId: null,
-        //    gifts: null,
-        //    groups: null,
-        //    id: 1
-        //});
+        let gifts: Gift[];
+        for (var i = 0; i < 5; i++) {
+            var gift = new Gift({
+                title: "Cool Gift",
+                description: "Description",
+                url: "http://www.cool.com",
+                userId: 1,
+                id: i
+            })
+            this.giftClient.post(gift);
+        }
+    }
 
-        //let gifts: Gift[];
-        //for (var i = 0; i < 5; i++) {
-        //    var gift = new Gift({
-        //        title: "Cool Gift",
-        //        description: "Description",
-        //        url: "http://www.cool.com",
-        //        userId: 1,
-        //        id: i
-        //    })
-        //    this.giftClient.post(gift);
-        //}
+    async createUser() {
+        var user = new User({
+            firstName: "Billy",
+            lastName: "Bob",
+            santaId: 1,
+            gifts: null,
+            groups: null,
+            id: 1
+        });
     }
 
     async deleteGifts() {
-        //var gifts = await this.retrieveGifts();
-        //for (var i = 0; i < gifts.length; i++) {
-        //    await this.giftClient.delete(gifts[i].id);
-        //}
+        var gifts = await this.retrieveGifts();
+        for (var i = 0; i < gifts.length; i++) {
+            await this.giftClient.delete(gifts[i].id);
+        }
     }
 
     async retrieveGifts() {
-        //var gifts = await this.giftClient.getAll();
-        //return gifts;
+        var gifts = await this.giftClient.getAll();
+        return gifts;
     }
 }
