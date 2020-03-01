099fdc847e463e9462da7e73706f988817f9bd71
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-03-01 14:44:38 -0800

    Users vue workings

--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -1,23 +1,14 @@
 ﻿import '../styles/site.scss';
-import { App } from './app';
-import { Gift } from './secretsanta-client.g';
+
+import Vue from 'vue';
+
+import Blah from './blah.vue';
+import UsersComponent from './components/User/usersComponent.vue';
 
 document.addEventListener("DOMContentLoaded", async () => {
-    let app = new App.Main();
-
-    await app.deleteGifts();
-
-    await app.createUser();
-
-    await app.createGifts();
-
-    let gifts = await app.getGifts();
-
-    let element = document.getElementById('giftList');
-
-    for (let gift of gifts) {
-        let liElement = element.appendChild(document.createElement('li'));
-        liElement.textContent = `${gift.id} ${gift.title} ${gift.description} ${gift.url}`;
+    if (document.getElementById('UserPage')) {
+        new Vue({
+            render: h => h(UsersComponent)
+        }).$mount('#UserPage');
     }
-    
 });
