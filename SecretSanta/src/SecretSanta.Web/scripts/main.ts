e7ae1fe7a91ad9823f7b1f9dbca7628499b7301c
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-03-01 16:27:04 -0800

    gift vue mostly working... kinda

--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -5,6 +5,7 @@ import Vue from 'vue';
 import Blah from './blah.vue';
 import UsersComponent from './components/User/usersComponent.vue';
 import GroupsComponent from './components/Group/groupsComponent.vue';
+import GiftsComponent from './components/Gift/giftsComponent.vue';
 
 document.addEventListener("DOMContentLoaded", async () => {
     if (document.getElementById('UserPage')) {
@@ -16,5 +17,10 @@ document.addEventListener("DOMContentLoaded", async () => {
         new Vue({
             render: h => h(GroupsComponent)
         }).$mount('#GroupPage');
+    }
+    else if (document.getElementById('GiftPage')) {
+        new Vue({
+            render: h => h(GiftsComponent)
+        }).$mount('#GiftPage');
     }
 });
