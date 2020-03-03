bd36c51f595ee59ebd0a49ee83009fef6f67875b
David Sergio <davidsergio52@gmail.com>
2020-03-02 21:27:05 -0800

    Assignment8

--- SecretSanta/src/SecretSanta.Web/scripts/app.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/app.ts
@@ -1,15 +1,81 @@
-﻿import { GiftClient, Gift, UserClient, User } from './secretsanta-client';
+﻿import { GiftClient, Gift, UserClient, User, GroupClient, GiftInput, UserInput, GroupInput, Group } from './secretsanta-client';
 
 export module App {
     export class Main {
         giftClient: GiftClient;
         userClient: UserClient;
+        groupClient: GroupClient;
         createdUser: User;
 
         constructor() {
             this.giftClient = new GiftClient('https://localhost:44388');
             this.userClient = new UserClient('https://localhost:44388');
+            this.groupClient = new GroupClient('https://localhost:44388');
         }
+
+        async insertGift(giftInput: GiftInput): Promise<Gift> {
+            var gift = await this.giftClient.post(giftInput);
+            return gift;
+        }
+        async getGifts(): Promise<Gift[]> {
+            var gifts = await this.giftClient.getAll();
+            return gifts;
+        }
+        async getGift(id: number): Promise<Gift> {
+            var gift = await this.giftClient.get(id);
+            return gift;
+        }
+        async updateGift(gift: Gift): Promise<Gift> {
+            var gift = await this.giftClient.put(gift.id, gift);
+            return gift;
+        }
+        async deleteGift(id: number): Promise<void> {
+            await this.giftClient.delete(id);
+        }
+
+        async insertUser(userInput: UserInput): Promise<User> {
+            var user = await this.userClient.post(userInput);
+            return user;
+        }
+        async getUsers(): Promise<User[]> {
+            var users = await this.userClient.getAll();
+            return users;
+        }
+        async getUser(id: number): Promise<User> {
+            var user = await this.userClient.get(id);
+            return user;
+        }
+        async updateUser(user: User): Promise<User> {
+            var user = await this.userClient.put(user.id, user);
+            return user;
+        }
+        async deleteUser(id: number): Promise<void> {
+            await this.userClient.delete(id);
+        }
+
+        async insertGroup(groupInput: GroupInput): Promise<Group> {
+            var group = await this.groupClient.post(groupInput);
+            return group;
+        }
+        async getGroups(): Promise<Group[]> {
+            var groups = await this.groupClient.getAll();
+            return groups;
+        }
+        async getGroup(id: number): Promise<Group> {
+            var geoup = await this.groupClient.get(id);
+            return geoup;
+        }
+        async updateGroup(group: Group): Promise<Group> {
+            var group = await this.groupClient.put(group.id, group);
+            return group;
+        }
+        async deleteGroup(id: number): Promise<void> {
+            await this.groupClient.delete(id);
+        }
+
+
+
+
         async deleteGifts() {
             var gifts = await this.getGifts();
 
@@ -28,12 +94,6 @@ export module App {
 
                 await this.giftClient.post(gift);
             }
-        }
-
-        async getGifts(): Promise<Gift[]> {
-            var gifts = await this.giftClient.getAll();
-
-            return gifts;
         }
 
         async createUser() {
--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -1,23 +1,37 @@
 ﻿import '../styles/site.scss';
-import { App } from './app';
-import { Gift } from './secretsanta-client';
+
+import Vue from 'vue';
+
+import GiftsComponent from './components/Gift/giftsComponent.vue';
 
 document.addEventListener("DOMContentLoaded", async () => {
+
+    if (document.getElementById('giftList')) {
+        new Vue({
+            render: h => h(GiftsComponent)
+        }).$mount('#giftList');
+    }
+});
+
+
+
+//import '../styles/site.scss';
+import { App } from './app';
+//import { Gift } from './secretsanta-client';
+
+document.addEventListener("DOMContentLoaded", async () => {
+
     let app = new App.Main();
 
-    await app.deleteGifts();
+    //await app.deleteGifts();
+    //await app.createUser();
+    //await app.createGifts();
 
-    await app.createUser();
-
-    await app.createGifts();
-
-    let gifts = await app.getGifts();
-
-    let element = document.getElementById('giftList');
-
-    for (let gift of gifts) {
-        let liElement = element.appendChild(document.createElement('li'));
-        liElement.textContent = `${gift.id} ${gift.title} ${gift.description} ${gift.url}`;
-    }
+    //let gifts = await app.getGifts();
+    //let element = document.getElementById('giftList');
+    //for (let gift of gifts) {
+    //    let liElement = element.appendChild(document.createElement('li'));
+    //    liElement.textContent = `${gift.id} ${gift.title} ${gift.description} ${gift.url}`;
+    //}
     
 });
