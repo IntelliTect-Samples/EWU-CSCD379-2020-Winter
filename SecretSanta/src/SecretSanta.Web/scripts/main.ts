4c77bf1c02c939efec79d3435338d232f4be1083
CDobbins1 <cdobsc01@hotmail.com>
2020-03-03 20:51:24 -0800

    Added gift and group vue

--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -1,31 +1,25 @@
 ﻿import '../styles/site.scss';
-import { App } from './app';
-import { Gift } from './secretsanta-client';
 import Vue from 'vue';
 import UsersComponent from './components/User/usersComponent.vue';
+import GroupsComponent from './components/Group/groupsComponent.vue';
+import GiftsComponent from './components/Gift/giftsComponent.vue';
+
 document.addEventListener("DOMContentLoaded", async () => {
     if (document.getElementById('userList')) {
         new Vue({
             render: h => h(UsersComponent)
         }).$mount('#userList');
     }
+
+    if (document.getElementById('groupList')) {
+        new Vue({
+            render: h => h(GroupsComponent)
+        }).$mount('#groupList');
+    }
+
+    if (document.getElementById('giftList')) {
+        new Vue({
+            render: h => h(GiftsComponent)
+        }).$mount('#giftList');
+    }
 });
-//document.addEventListener("DOMContentLoaded", async () => {
-//    let app = new App.Main();
-
-//    await app.deleteGifts();
-
-//    await app.createUser();
-
-//    await app.createGifts();
-
-//    let gifts = await app.getGifts();
-
-//    let element = document.getElementById('giftList');
-
-//    for (let gift of gifts) {
-//        let liElement = element.appendChild(document.createElement('li'));
-//        liElement.textContent = `${gift.id} ${gift.title} ${gift.description} ${gift.url}`;
-//    }
-    
-//});
