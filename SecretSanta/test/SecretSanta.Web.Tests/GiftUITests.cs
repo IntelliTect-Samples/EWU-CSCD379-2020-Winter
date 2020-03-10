c7171b2e370cb9dfa2e08c704b6fa87326ef484a
David Sergio <davidsergio52@gmail.com>
2020-03-09 18:07:22 -0700

    Assignment9

--- SecretSanta/test/SecretSanta.Web.Tests/GiftUITests.cs
+++ SecretSanta/test/SecretSanta.Web.Tests/GiftUITests.cs
@@ -28,93 +28,6 @@ namespace SecretSanta.Web.Tests
 		public IWebDriver? WebDriver { get; set; }
 		private static Process? ApiHostProcess { get; set; }
 		private static Process? WebHostProcess { get; set; }
-		public string MyUrl { get; set; } = "http://www.duckduckgo.com";
-
-        //        [ClassInitialize]
-        //        public static void ClassInitialize(TestContext testContext)
-        //        {
-
-        //            using WebClient webClient = new WebClient();
-
-        //            ApiHostProcess = StartWebHost("SecretSanta.Api", 44388, "Swagger", new string[] { "ConnectionStrings:DefaultConnection='Data Source=SecretSanta.db'" });
-
-        //            WebHostProcess = StartWebHost("SecretSanta.Web", 44394, "", " ApiUrl=https://localhost:44394");
-
-        //            Process StartWebHost(string projectName, int port, string urlSubDirectory, params string[] args)
-        //            {
-
-        //                string fileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, projectName + ".exe");
-        //                Process[] alreadyExecutingProcesses = Process.GetProcessesByName(projectName);
-        //                if (alreadyExecutingProcesses.Length != 0)
-        //                {
-        //                    foreach (Process item in alreadyExecutingProcesses)
-        //                    {
-        //                        item.Kill();
-        //                    }
-        //                }
-
-        //                string testAssemblyLocation = Assembly.GetExecutingAssembly().Location;
-        //                string testAssemblyName = Path.GetFileNameWithoutExtension(testAssemblyLocation);
-        //                string projectExe = testAssemblyLocation.RegexReplace(testAssemblyName, projectName).RegexReplace(@"\\test\\", @"\src\").RegexReplace("dll$", "exe");
-
-        //                string argumentList = $"{string.Join(" ", args)} Urls=https://localhost:{port}";
-
-        //                ProcessStartInfo startInfo = new ProcessStartInfo(projectExe, argumentList)
-        //                {
-        //                    RedirectStandardError = true,
-        //                    RedirectStandardOutput = true,
-        //                    UseShellExecute = false,
-        //                    CreateNoWindow = true
-        //                };
-
-        //                string stdErr = "";
-        //                string stdOut = "";
-        //                // Justification: Dispose invoked by caller on Process object returned.
-        //#pragma warning disable CA2000 // Dispose objects before losing scope
-        //                Process host = new Process
-        //                {
-        //                    EnableRaisingEvents = true,
-        //                    StartInfo = startInfo
-        //                };
-        //#pragma warning restore CA2000 // Dispose objects before losing scope
-
-        //                host.ErrorDataReceived += (sender, args) =>
-        //                    stdErr += $"{args.Data}\n";
-        //                host.OutputDataReceived += (sender, args) =>
-        //                    stdOut += $"{args.Data}\n";
-        //                host.Start();
-        //                host.BeginErrorReadLine();
-        //                host.BeginOutputReadLine();
-
-        //                for (int seconds = 20; seconds > 0; seconds--)
-        //                {
-        //                    if (stdOut.Contains("Application started."))
-        //                    {
-        //                        _ = webClient.DownloadString(
-        //                            $"https://localhost:{port}/{urlSubDirectory.TrimStart(new char[] { '/', '\\' })}");
-        //                        return host;
-        //                    }
-        //                    else if (host.WaitForExit(1000))
-        //                    {
-        //                        break;
-        //                    }
-        //                }
-
-        //                if (!host.HasExited) host.Kill();
-        //                host.WaitForExit();
-        //                throw new InvalidOperationException($"Unable to execute process successfully: {stdErr}") { Data = { { "StandardOut", stdOut } } };
-
-        //            }
-        //        }
-
-        //        [ClassCleanup]
-        //        public static void ClassCleanup()
-        //        {
-        //            ApiHostProcess?.CloseMainWindow();
-        //            ApiHostProcess?.Close();
-        //            WebHostProcess?.CloseMainWindow();
-        //            WebHostProcess?.Close();
-        //        }
 
         [ClassInitialize]
         public static void ClassInitalize(TestContext testContext)
@@ -162,17 +75,6 @@ namespace SecretSanta.Web.Tests
 		{
 			WebDriver.Quit();
 		}
-
-		//[TestMethod]
-		//public void MyTestMethod()
-		//{
-		//	WebDriver.Navigate().GoToUrl(MyUrl);
-		//	var input = WebDriver.FindElement(By.Id("search_form_input_homepage"));
-		//	input.SendKeys("selenium");
-		//	var button = WebDriver.FindElement(By.Id("search_button_homepage"));
-		//	button.Click();
-
-		//}
 
         [TestMethod]
         public void VerifySiteIsUp()
